6d1a6677284db70a7705eb279effbaa6196fb805
SunnY <nakerin7588@gmail.com>
2025-03-11 16:30:41 +0700

    Update bno055_usb_stick_node_script.py

--- src/bno055_usb_stick/scripts/bno055_usb_stick_node_script.py
+++ src/bno055_usb_stick/scripts/bno055_usb_stick_node_script.py
@@ -5,6 +5,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Imu
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSDurabilityPolicy
 from bno055_usb_stick_py import BnoUsbStick
+import numpy as np
 
 class BNO055USBSTICKNode(Node):
     def __init__(self):
@@ -18,24 +19,34 @@ class BNO055USBSTICKNode(Node):
         self.timer = self.create_timer(1/30, self.timer_callback)
         
         self.bno_usb_stick = BnoUsbStick(port='/dev/ttyACM0')
+        self.prev_gyro = [0, 0, 0]
 
     def timer_callback(self):
         self.bno_usb_stick.activate_streaming()
         for package in self.bno_usb_stick.recv_streaming_generator(num_packets=1):
-            imu_msg = Imu()
-            imu_msg.header.stamp = self.get_clock().now().to_msg()
-            imu_msg.header.frame_id = 'imu_link'
-            imu_msg.orientation.x = package.quaternion[1]
-            imu_msg.orientation.y = package.quaternion[2]
-            imu_msg.orientation.z = package.quaternion[3]
-            imu_msg.orientation.w = package.quaternion[0]
-            imu_msg.angular_velocity.x = package.g[0]
-            imu_msg.angular_velocity.y = package.g[1]
-            imu_msg.angular_velocity.z = package.g[2]
-            imu_msg.linear_acceleration.x = package.lin_a[0]
-            imu_msg.linear_acceleration.y = package.lin_a[1]
-            imu_msg.linear_acceleration.z = package.lin_a[2]
-            self.publisher.publish(imu_msg)
+            # I use data from gyro for fusion with wheel odometry, So i will check the data error from gyro only.
+            if np.any(np.array(package.g) >= 300):
+                # print('Error data from gyro')
+                # print(self.prev_gyro)
+                break
+            else:
+                self.prev_gyro = package.g
+                # print(self.prev_gyro)
+        
+        imu_msg = Imu()
+        imu_msg.header.stamp = self.get_clock().now().to_msg()
+        imu_msg.header.frame_id = 'imu_link'
+        imu_msg.orientation.x = package.quaternion[1]
+        imu_msg.orientation.y = package.quaternion[2]
+        imu_msg.orientation.z = package.quaternion[3]
+        imu_msg.orientation.w = package.quaternion[0]
+        imu_msg.angular_velocity.x = package.g[0]
+        imu_msg.angular_velocity.y = package.g[1]
+        imu_msg.angular_velocity.z = package.g[2]
+        imu_msg.linear_acceleration.x = package.lin_a[0]
+        imu_msg.linear_acceleration.y = package.lin_a[1]
+        imu_msg.linear_acceleration.z = package.lin_a[2]
+        self.publisher.publish(imu_msg)
         
 def main(args=None):
     rclpy.init(args=args)
